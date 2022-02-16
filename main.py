b61d034c6f150029ff112e9cfd6bff8aa66ca81f
Lucas do Drip <77930083+lucascompython@users.noreply.github.com>
2022-02-16 22:35:57 +0000

    Main file

---
+++ main.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request
+
+
+
+import re
+from sympy import Matrix, lcm
+
+
+
+elementList = []
+elementMatrix = []
+
+# print("please input your reactants, this is case sensitive")
+# print("your input should look like: H2O+Ag3(Fe3O)4")
+
+# reactants=input("Reactants: ")
+
+# print("please input your products, this is case sensitive")
+
+# products=input("Products: ")
+# reactants=reactants.replace(' ', '').split("+")
+# products=products.replace(' ', '').split("+")
+
+def addToMatrix(element, index, count, side):
+    global elementMatrix, elementList
+
+    
+    if(index == len(elementMatrix)):
+       elementMatrix.append([])
+
+       for x in elementList:
+            elementMatrix[index].append(0)
+
+    if(element not in elementList):
+        elementList.append(element)
+
+        for i in range(len(elementMatrix)):
+            elementMatrix[i].append(0)
+
+    column=elementList.index(element)
+    elementMatrix[index][column] += count * side
+
+def findElements(segment,index, multiplier, side):
+    elementsAndNumbers=re.split('([A-Z][a-z]?)',segment)
+    i = 0
+    while(i < len(elementsAndNumbers) - 1):#last element always blank
+          i += 1
+          if(len(elementsAndNumbers[i]) > 0):
+            if(elementsAndNumbers[i+1].isdigit()):
+                count=int(elementsAndNumbers[i+1])*multiplier
+                addToMatrix(elementsAndNumbers[i], index, count, side)
+                i += 1
+            else:
+                addToMatrix(elementsAndNumbers[i], index, multiplier, side)
+
+def compoundDecipher(compound, index, side):
+    segments=re.split('(\([A-Za-z0-9]*\)[0-9]*)',compound)
+    for segment in segments:
+        if segment.startswith("("):
+            segment = re.split('\)([0-9]*)', segment)
+            multiplier = int(segment[1])
+            segment = segment[0][1:]
+        else:
+            multiplier = 1
+        findElements(segment, index, multiplier, side)
+
+
+
+
+
+app = Flask('app')
+
+@app.route('/', methods = ["POST", "GET"])
+def hello_world():
+  global elementMatrix, elementList
+  if request.method == "GET":
+    
+    return render_template("index.html")
+  elif request.method == "POST":
+    
+    form_data = dict(request.form)
+    print(form_data)
+    reactants = str(form_data["reactants"])
+    products = str(form_data["products"])
+    reactants = reactants.replace(' ', '').split("+")
+    products = products.replace(' ', '').split("+")
+
+    #inicio
+    for i in range(len(reactants)):
+        compoundDecipher(reactants[i], i, 1)
+    
+    for i in range(len(products)):
+        compoundDecipher(products[i], i + len(reactants), -1)
+      
+    
+    
+    elementMatrix = Matrix(elementMatrix)
+    elementMatrix = elementMatrix.transpose()
+    try:
+      
+      solution = elementMatrix.nullspace()[0]
+    except IndexError:
+      elementMatrix = []
+      elementList = []
+      return render_template("index.html", error="Os reagentes e os produtos não batem certo...")
+    multiple = lcm([val.q for val in solution])
+    solution = multiple * solution
+    coEffi = solution.tolist()
+    output = ""
+    for i in range(len(reactants)):
+        output += str(coEffi[i][0])+reactants[i]
+        if i < len(reactants) -1:
+           output += " + "
+    output+=" -> "
+    for i in range(len(products)):
+       output += str(coEffi[i + len(reactants)][0]) + products[i]
+       if i < len(products) -1:
+           output+=" + "
+    print(output)
+    print(elementMatrix, elementList)
+    elementMatrix = []
+    elementList = []
+
+    
+    return render_template("index.html", form_data = form_data, output=output)
+  else:
+    return "FUCK ROBOTS"
+
+
+app.run(host='0.0.0.0', port=8080)
