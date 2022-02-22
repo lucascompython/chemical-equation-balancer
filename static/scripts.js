cc49df92a6723090aa23da2c114f15111f4e6997
Lucas do Drip <77930083+lucascompython@users.noreply.github.com>
2022-02-22 23:48:16 +0000

    Javascript to ajust the results

---
+++ static/scripts.js
@@ -0,0 +1,31 @@
+
+var checkIfExists = setInterval(() => {
+    var exists = document.getElementById("results");
+
+    if (exists) {
+        clearInterval(checkIfExists);
+        ajustResults(exists);
+    }
+}, 10);
+
+const ajustResults = (element) => {
+  const results = element.innerHTML.split(" ");
+
+  
+  const sliced = [];
+  console.log(results)
+  for (let elemento of results) {
+    if (elemento[0] === "1")
+      sliced.push(elemento.substring(1))
+    else {
+      sliced.push(elemento)
+    }
+    
+  }
+  
+
+  const regSubbed = sliced.join(" ").replace(/\B(\d+)/g, '<sub>$1</sub>');
+  const ajusted = document.getElementById("results").innerHTML = regSubbed;
+
+  console.log(ajusted)
+}
